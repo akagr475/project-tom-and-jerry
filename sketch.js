af19da757f628441b74f5cf2956eb1860c62846c
akagr475 <76026968+akagr475@users.noreply.github.com>
2021-04-26 23:55:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,64 @@
 
-var Tom, Jerry;
-var BgImg, mouse1Img, mouse2Img, mouse3Img, cat1Img, cat2Img, cat3Img;
 
+var cat, rat;
+var backgroundImg;
 
-function preload() {
-  cat1Img = loadImage("images/cat1.png");
-  cat2Img = loadImage("images/cat2.png","images/cat3.png");
-  cat3Img = loadImage("images/cat4.png")
-
-  BgImg = loadImage("images/garden.png")
-
-  mouse1Img = loadImage("images/mouse1.png");
-  mouse2Img = loadImage("images/mouse3.png","images/mouse2.png");
-  mouse3Img = loadImage("images/mouse4.png")
+function preload()
+ {
+    backgroundImg=loadImage("images/garden.png");
+    catImg   = loadImage("images/cat1.png");
+    mouseImg = loadImage("images/mouse1.png");
+    catImg2  = loadAnimation("images/cat2.png","images/cat3.png");
+    catImg3=loadAnimation("images/cat4.png");
+    mouseImg2= loadAnimation("images/mouse2.png","images/mouse3.png");
+    mouseImg3=loadImage("images/mouse4.png");
 }
 
-function setup(){
+function setup()
+{
     createCanvas(1000,800);
-
-     Tom = createSprite(800,650,10,10);
-     Tom.addImage("cat",cat1Img);
-     Tom.scale = 0.2;
-
-     Jerry = createSprite(100,650,10,10);
-     Jerry.addImage("mouse",mouse1Img);
-     Jerry.scale = 0.2;
+    
+    cat=createSprite(800,700,50,50);
+    cat.addImage(catImg);
+    cat.scale=0.2
+    rat=createSprite(200,700,50,50);
+    rat.addImage(mouseImg);
+    rat.scale=0.15
 }
 
-function draw() {
+function draw() 
+{
 
-    background(BgImg);
+    background(backgroundImg);
+    
+    if(cat.x - rat.x<(cat.width - rat.width)/2)
+    {
+        cat.velocityX=0;
+        cat.addAnimation("catlast",catImg3);
+        cat.changeAnimation("catlast");
+        cat.x=300;
+        cat.scale=0.2;
 
-    if(Tom.x == 270){
-      Tom.addImage("cat",cat3Img);
-      Tom.changeAnimation("cat",cat3Img);
-      Tom.velocityX = 0;
 
-      Jerry.addImage("mouse",mouse3Img);
-      Jerry.changeAnimation("mouse",mouse3Img);
+        rat.addAnimation("lastImg",mouseImg3);
+        rat.scale=0.15;
+        rat.changeAnimation("lastImg");
     }
-     
-    keyPressed();
 
     drawSprites();
 }
 
+function keyPressed()
+{
+    if(keyCode === LEFT_ARROW)
+    {
 
-function keyPressed(){
-
-  
-      if(keyDown("LEFT_ARROW")){
-          Tom.velocityX = -2;
-          Tom.addImage("cat",cat2Img);
-          Tom.changeAnimation("cat",cat2Img);
-
-          Jerry.addImage("mouse",mouse2Img);
-          Jerry.changeAnimation("mouse",mouse2Img);
-      }
-
-}
+     rat.addAnimation("mouseTeasing",mouseImg2);
+     rat.frameDelay=25;
+     rat.changeAnimation("mouseTeasing");
+     
+     cat.addAnimation("catRunning",catImg2);
+     cat.changeAnimation("catRunning");  
+     cat.velocityX=-5;
+    }
+  }
